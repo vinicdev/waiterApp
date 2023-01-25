d5f45c616e3e317ae5800ea3b75e324f051edfed
Vinicius Cardoso <75495504+vinicdev@users.noreply.github.com>
2023-01-24 21:51:29 -0300

    add app

---
+++ app/src/components/Cart/index.tsx
@@ -0,0 +1,134 @@
+import { useState } from 'react';
+import { FlatList, TouchableOpacity } from 'react-native';
+import { CartItem } from '../../types/CartItem';
+import { Product } from '../../types/Product';
+import { api } from '../../utils/api';
+import { formatCurrency } from '../../utils/formatCurrency';
+import { Button } from '../Button';
+import { MinusCircle } from '../Icons/MinusCircle';
+import { PlusCircle } from '../Icons/PlusCircle';
+import { OrderConfirmedModal } from '../OrderConfirmedModal';
+import { Text } from '../Text';
+import {
+  Actions,
+  Item,
+  ProductContainer,
+  Image,
+  QuantityContainer,
+  ProductDetails,
+  Summary,
+  TotalContainer
+
+} from './styles';
+
+interface CartProps {
+  cartItems: CartItem[];
+  onAdd: (product: Product) => void;
+  onDecrement: (product: Product) => void;
+  onConfirmOrder: () => void;
+  selectedTable: string;
+}
+
+export function Cart({ cartItems, onAdd, onDecrement, onConfirmOrder, selectedTable }: CartProps) {
+  const [isLoading, setIsLoading] = useState(false);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const total = cartItems.reduce((acc, cartItems) => {
+    return acc + cartItems.quantity * cartItems.product.price;
+  }, 0);
+
+  async function handleConfirmOrder() {
+    setIsLoading(true);
+
+    const payload = {
+      table: selectedTable,
+      products: cartItems.map((cartItems) => ({
+        product: cartItems.product._id,
+        quantity: cartItems.quantity,
+      })),
+    };
+
+    await api.post('/orders', payload);
+    setIsLoading(false);
+    setIsModalVisible(true);
+  }
+
+  function handleOk() {
+    onConfirmOrder();
+    setIsModalVisible(false);
+  }
+
+  return (
+    <>
+      <OrderConfirmedModal
+        visible={isModalVisible}
+        onOk={handleOk}
+      />
+      {cartItems.length > 0 && (
+        <FlatList
+          data={cartItems}
+          keyExtractor={cartItems => cartItems.product._id}
+          showsHorizontalScrollIndicator={false}
+          style={{ marginBottom: 20, maxHeight:  140}}
+          renderItem={({ item: cartItem }) => (
+            <Item>
+              <ProductContainer>
+                <Image
+                  source={{
+                    uri: `http://192.168.100.197:3001/uploads/${cartItem.product.imagePath}`,
+                  }}
+                />
+
+                <QuantityContainer>
+                  <Text size={14} color="#666">
+                    {cartItem.quantity}x
+                  </Text>
+                </QuantityContainer>
+
+                <ProductDetails>
+                  <Text size={14} weight="600">{cartItem.product.name}</Text>
+                  <Text size={14} color="#666" style={{ marginTop: 4 }}>
+                    {formatCurrency(cartItem.product.price)}
+                  </Text>
+                </ProductDetails>
+              </ProductContainer>
+
+              <Actions>
+                <TouchableOpacity
+                  style={{ marginRight: 24 }}
+                  onPress={() => onAdd(cartItem.product)}
+                >
+                  <PlusCircle></PlusCircle>
+                </TouchableOpacity>
+
+                <TouchableOpacity onPress={() => onDecrement(cartItem.product)}>
+                  <MinusCircle></MinusCircle>
+                </TouchableOpacity>
+              </Actions>
+            </Item>
+          )}
+        />
+      )}
+
+      <Summary>
+        <TotalContainer>
+          {cartItems.length > 0 ?(
+            <>
+              <Text color="#666">Total</Text>
+              <Text size={20} weight="600">{formatCurrency(total)}</Text>
+            </>
+          ) : (
+            <Text color="#999">Seu carrinho está vazio</Text>
+          )}
+        </TotalContainer>
+
+        <Button
+          onPress={handleConfirmOrder}
+          disabled={cartItems.length === 0}
+          loading={isLoading}
+        >
+          Confirmar pedido
+        </Button>
+      </Summary>
+    </>
+  );
+}
---
+++ app/src/mocks/products.ts
@@ -0,0 +1,87 @@
+export const products = [
+  {
+    _id: '6372e040f52e37ef85fe2c5e',
+    name: 'Pizza quatro queijos',
+    description: 'Deliciosa pizza quatro queijos com borda simples',
+    imagePath: '1670096034415-quatro-queijos.png',
+    price: 40,
+    ingredients: [
+      {
+        name: 'Mussarela',
+        icon: '🧀',
+        _id: '6372e040f52e37ef85fe2c5f'
+      },
+      {
+        name: 'Parmesão',
+        icon: '🧀',
+        _id: '6372e040f52e37ef85fe2c60'
+      },
+      {
+        name: 'Gouda',
+        icon: '🧀',
+        _id: '6372e040f52e37ef85fe2c61'
+      },
+      {
+        name: 'Brie',
+        icon: '🧀',
+        _id: '6372e040f52e37ef85fe2c62'
+      }
+    ],
+  },
+  {
+    _id: '6372e276a381106c0f854cb3',
+    name: 'Coca cola',
+    description: 'Coca cola lata geladinha topzera',
+    imagePath: '1670097034776-coca-cola.png',
+    price: 7,
+    ingredients: [],
+  },
+  {
+    _id: '6372e276a381106c0f2854cb3',
+    name: 'Coca cola',
+    description: 'Coca cola lata geladinha topzera',
+    imagePath: '1670097034776-coca-cola.png',
+    price: 7,
+    ingredients: [],
+  },
+  {
+    _id: '6372e276a381106c320f854cb3',
+    name: 'Coca cola',
+    description: 'Coca cola lata geladinha topzera',
+    imagePath: '1670097034776-coca-cola.png',
+    price: 7,
+    ingredients: [],
+  },
+  {
+    _id: '6372e276a381106c0f8454cb3',
+    name: 'Coca cola',
+    description: 'Coca cola lata geladinha topzera',
+    imagePath: '1670097034776-coca-cola.png',
+    price: 7,
+    ingredients: [],
+  },
+  {
+    _id: '6372e276a38110655c0f854cb3',
+    name: 'Coca cola',
+    description: 'Coca cola lata geladinha topzera',
+    imagePath: '1670097034776-coca-cola.png',
+    price: 7,
+    ingredients: [],
+  },
+  {
+    _id: '6372e276a381106c660f854cb3',
+    name: 'Coca cola',
+    description: 'Coca cola lata geladinha topzera',
+    imagePath: '1670097034776-coca-cola.png',
+    price: 7,
+    ingredients: [],
+  },
+  {
+    _id: '6372e276a38110677c0f854cb3',
+    name: 'Coca cola',
+    description: 'Coca cola lata geladinha topzera',
+    imagePath: '1670097034776-coca-cola.png',
+    price: 7,
+    ingredients: [],
+  },
+];
